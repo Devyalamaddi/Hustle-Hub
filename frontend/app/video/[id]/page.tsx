6505aa7a8960b463014e22988a63247013827a1c
Devendra Yalamaddi <devendrayalamaddi@gmail.com>
2025-05-01 19:14:30 +0530

    fix: few issues

--- frontend/app/video/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import VideoMeeting from "@/components/video/video-meeting"
-import { useParams } from "next/navigation"
-
-export default function VideoMeetingPage() {
-  const {id} = useParams();
-  return <VideoMeeting roomId={id.toString()} />
-}
